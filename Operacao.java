9d91a38e9168ebfbeb2471c84e4194570ebe6091
Caio Souza <113695603+Caiofz@users.noreply.github.com>
2023-11-19 01:55:34 -0300

    Create Operacao.java

---
+++ Operacao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cofin;
+
+/**
+ *
+ * @author -
+ */
+public abstract class Operacao {
+    private String nome;
+    private float valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+}
