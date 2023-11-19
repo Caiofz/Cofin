737390e3c01ce0b6cc427693170ffb4ed696106f
Caio Souza <113695603+Caiofz@users.noreply.github.com>
2023-11-19 01:55:16 -0300

    Create Cofin.java

---
+++ Cofin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cofin;
+
+/**
+ *
+ * @author -
+ */
+public class Cofin {
+
+    public static void main(String[] args) {
+        
+    }
+}
